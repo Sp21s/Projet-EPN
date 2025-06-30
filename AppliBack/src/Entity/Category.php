d7c5bb26de49a9c0d44633cc191379c90f8726da
LePelicaan <romain.guibat@gmail.com>
2025-06-30 11:21:42 +0200

    Plein de modif

--- AppliBack/src/Entity/Category.php
+++ AppliBack/src/Entity/Category.php
@@ -3,25 +3,37 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
 {
-    /*
+    
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-    */
-    #[ORM\Id]
+    
     #[ORM\Column(length: 255)]
     private ?string $libelle = null;
 
-    /*public function getId(): ?int
+    /**
+     * @var Collection<int, Job>
+     */
+    #[ORM\OneToMany(targetEntity: Job::class, mappedBy: 'category')]
+    private Collection $jobs;
+
+    public function __construct()
+    {
+        $this->jobs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
     {
         return $this->id;
-    }*/
+    }
 
     public function getLibelle(): ?string
     {
@@ -31,6 +43,36 @@ class Category
     public function setLibelle(string $libelle): static
     {
         $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Job>
+     */
+    public function getJobs(): Collection
+    {
+        return $this->jobs;
+    }
+
+    public function addJob(Job $job): static
+    {
+        if (!$this->jobs->contains($job)) {
+            $this->jobs->add($job);
+            $job->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeJob(Job $job): static
+    {
+        if ($this->jobs->removeElement($job)) {
+            // set the owning side to null (unless already changed)
+            if ($job->getCategory() === $this) {
+                $job->setCategory(null);
+            }
+        }
 
         return $this;
     }
